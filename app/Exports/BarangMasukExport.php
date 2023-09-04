d3d8335daa5c251fc2c8967daf2354a51672e953
Reignleiv021 <rizalp021@gmail.com>
2023-09-04 15:02:04 +0700

    Fix Barang Masuk

--- app/Exports/BarangMasukExport.php
+++ app/Exports/BarangMasukExport.php
@@ -8,19 +8,30 @@ use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\FromQuery;
 use Maatwebsite\Excel\Concerns\Exportable;
 use Illuminate\Support\Facades\DB;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Carbon\Carbon;
+use Maatwebsite\Excel\Events\AfterSheet;
 
-class BarangMasukExport implements FromCollection, WithHeadings
+class BarangMasukExport implements FromQuery, WithHeadings, ShouldAutoSize
 {
     /**
      * @return \Illuminate\Support\Collection
      */
+    use Exportable;
 
-    public function collection()
+    protected $tglawal;
+    protected $tglakhir;
+    public function __construct($tglawal, $tglakhir)
     {
-        return BarangmasukModel::all();
+        $this->tglawal = $tglawal;
+        $this->tglakhir = $tglakhir;
     }
     public function headings(): array
     {
         return ["Id", "Kode Barang Masuk", "Kode Barang", "Supplier", "Tanggal Masuk", "Jumlah Barang Masuk", "Density", "Nomor Polisi", "Nomor Surat Jalan", "Jumlah Masuk Actual"];
+    }
+    public function query()
+    {
+        return BarangmasukModel::query();
     }
 }
